9b4fd272c8ee15075ed6ad7b098c448917ab7267
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-10-02 03:47:20 -0300

    Initialize printscript-service with Gradle and Spring Boot setup

---
+++ src/main/kotlin/com/ingsis/grupo10/printscript/PrintscriptApplication.kt
@@ -0,0 +1,11 @@
+package com.ingsis.grupo10.printscript
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrintscriptApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrintscriptApplication>(*args)
+}
---
+++ src/test/kotlin/com/ingsis/grupo10/printscript/PrintscriptApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis.grupo10.printscript
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrintscriptApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
